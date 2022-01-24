b65193ee8057fe6b56839be0d41678d057d951d9
PrismSpirit <yeolkeut@gmail.com>
2022-01-24 17:06:19 +0900

    Initial Commit

---
+++ GuaranteeWallet/GuaranteeWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuaranteeWalletApp.swift
+//  GuaranteeWallet
+//
+//  Created by Jaehun Lee on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GuaranteeWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
